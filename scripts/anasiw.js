6033cec4f9f690a0a3e06bc4393221ab8d1ccca6
Zouhir OUFTOU <zouhir.ouftou@gmail.com>
2026-01-03 21:07:20 +0100

    update design

--- scripts/anasiw.js
+++ scripts/anasiw.js
@@ -1,4 +1,4 @@
-const textarea = document.getElementById('ansa');
+const textarea = document.getElementById('tirra');
 const keyboard = document.getElementById('charKeyboard');
 
 keyboard.addEventListener('click', function (e) {
@@ -17,3 +17,57 @@ keyboard1.addEventListener('click', function (e) {
         textarea.value += e.target.dataset.char;
     }
 });
+
+function directMode() {
+    let textarea = document.getElementById("tirra").value;
+    let lastchar = textarea.charAt(textarea.length - 1);
+    let lastsecondchar = textarea.charAt(textarea.length - 2);
+    if (lastchar == '=') {
+        switch (lastsecondchar) {
+            case 'c':
+                document.getElementById("tirra").value = textarea.substring(0, textarea.length - 2) + 'č';
+                break;
+            case 'd':
+                document.getElementById("tirra").value = textarea.substring(0, textarea.length - 2) + 'ḍ';
+                break;
+            case 'g':
+                document.getElementById("tirra").value = textarea.substring(0, textarea.length - 2) + 'ǧ';
+                break;
+            case 'h':
+                document.getElementById("tirra").value = textarea.substring(0, textarea.length - 2) + 'ḥ';
+                break;
+            case 'r':
+                document.getElementById("tirra").value = textarea.substring(0, textarea.length - 2) + 'ṛ';
+                break;
+            case 's':
+                document.getElementById("tirra").value = textarea.substring(0, textarea.length - 2) + 'ṣ';
+                break;
+            case 't':
+                document.getElementById("tirra").value = textarea.substring(0, textarea.length - 2) + 'ṭ';
+                break;
+            case 'z':
+                document.getElementById("tirra").value = textarea.substring(0, textarea.length - 2) + 'ẓ';
+                break;
+            case 'e':
+                document.getElementById("tirra").value = textarea.substring(0, textarea.length - 2) + 'ɛ';
+                break;
+            case 'y':
+                document.getElementById("tirra").value = textarea.substring(0, textarea.length - 2) + 'ɣ';
+                break;
+            case 'E':
+                document.getElementById("tirra").value = textarea.substring(0, textarea.length - 2) + 'Σ';
+                break;
+            case 'Y':
+                document.getElementById("tirra").value = textarea.substring(0, textarea.length - 2) + 'Γ';
+                break;
+        }
+    }
+}
+
+function focusTextarea() {
+    document.getElementById('tirra').focus();
+}
+
+function appendChar(char) {
+    document.getElementById("tirra").value += char;
+}
