1af96e08cd7ef309ca2502f50c83c9f7af3d9c59
Zouhir OUFTOU <zouhir.ouftou@gmail.com>
2026-01-03 20:52:22 +0100

    update design

--- scripts/anasiw.js
+++ scripts/anasiw.js
@@ -1,7 +1,17 @@
 const textarea = document.getElementById('ansa');
-const keyboard1 = document.getElementById('charKeyboard');
+const keyboard = document.getElementById('charKeyboard');
 
 keyboard.addEventListener('click', function (e) {
+    if (e.target.dataset.char) {
+        textarea.focus();
+        textarea.value += e.target.dataset.char;
+    }
+});
+
+
+const keyboard1 = document.getElementById('charKeyboard1');
+
+keyboard1.addEventListener('click', function (e) {
     if (e.target.dataset.char) {
         textarea.focus();
         textarea.value += e.target.dataset.char;
